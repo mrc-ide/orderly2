ea8c201f7f0220778c94d077b43d530c337b6013
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-03-30 12:03:26 +0100

    Add docs

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -5,8 +5,8 @@ test_that("can read file with no helpers", {
 
 test_that("can read file with helpers", {
   dat <- orderly_read_r("examples/explicit/orderly.R")
-  expect_setequal(names(dat), c("resource", "artefacts"))
-  expect_equal(dat$resource, "data.csv")
+  expect_setequal(names(dat), c("resources", "artefacts"))
+  expect_equal(dat$resources, "data.csv")
   expect_equal(dat$artefacts,
                list(list(description = "A graph of things",
                          files = "mygraph.png")))
