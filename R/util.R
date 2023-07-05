4738008956351728682c993b909ba66c582c74ac
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-07-05 11:28:21 +0100

    Fix lint

--- R/util.R
+++ R/util.R
@@ -158,10 +158,8 @@ yaml_load <- function(string) {
   ## More restrictive true/false handling.  Only accept if it maps to
   ## full (true|yes) / (false|no):
   handlers <- list(
-    "bool#yes" = function(x)
-      if (tolower(x) %in% c("true", "yes")) TRUE else x,
-    "bool#no" = function(x)
-      if (tolower(x) %in% c("false", "no")) FALSE else x)
+    "bool#yes" = function(x) if (tolower(x) %in% c("true", "yes")) TRUE else x,
+    "bool#no" = function(x) if (tolower(x) %in% c("false", "no")) FALSE else x)
   yaml::yaml.load(string, handlers = handlers)
 }
 
