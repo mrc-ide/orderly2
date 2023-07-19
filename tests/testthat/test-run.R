3733b3d97c28f5f60848e821484c0bddf5062810
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-07-19 14:11:07 +0100

    More path drama

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -857,7 +857,7 @@ test_that("validation of orderly directories", {
   nms <- sprintf("example_%s", letters[1:8])
   fs::dir_create(file.path(path, "src", nms))
   file.create(file.path(path, "src", nms, "orderly.R"))
-  hint_root <- sprintf("Looked relative to orderly root at '%s'", path)
+  hint_root <- sprintf("Looked relative to orderly root at '%s'", root$path)
 
   err <- expect_error(
     validate_orderly_directory("foo", root),
