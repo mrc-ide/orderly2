cc7875af073b132f4d896b6ed993911774201b31
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-07-26 08:05:07 +0100

    Start merging orderly and outpack root

--- R/root.R
+++ R/root.R
@@ -10,18 +10,52 @@
 ##' @param path The path to initialise the repository at.  If the
 ##'   repository is already initialised, this operation does nothing.
 ##'
-##' @param ... Additional arguments passed through to
-##'   [orderly2::outpack_init], if (and only if) the outpack
-##'   repository is not already created.
+##' @param path_archive Path to the archive directory, used to store
+##'   human-readable copies of packets.  If `NULL`, no such copy is
+##'   made, and `file_store` must be `TRUE`
+##'
+##' @param use_file_store Logical, indicating if we should use a
+##'   content-addressable file-store as the source of truth for
+##'   packets.  If `archive` is non-`NULL`, the file-store will be
+##'   used as the source of truth and the duplicated files in archive
+##'   exist only for convenience.
+##'
+##' @param require_complete_tree Logical, indicating if we require a
+##'   complete tree of packets.  This currently affects
+##'   [orderly2::outpack_location_pull_packet], by requiring that it
+##'   always operates in recursive mode.  This is `FALSE` by default,
+##'   but set to `TRUE` if you want your archive to behave well as a
+##'   location; if `TRUE` you will always have all the packets that
+##'   you hold metadata about.
+##'
+##' @param logging_console Logical, indicating if we should log to the
+##'   console. If `TRUE`, then many operations will produce
+##'   informational output; set to `FALSE` to prevent this.
+##'
+##' @param logging_threshold The degree of verbosity; one of `info`,
+##'   `debug` or `trace` in increasing order of verbosity.
 ##'
 ##' @return An orderly root object, invisibly. Typically this is
 ##'   called only for its side effect.
 ##'
 ##' @export
-orderly_init <- function(path, ...) {
+orderly_init <- function(path,
+                         path_archive = "archive",
+                         use_file_store = FALSE,
+                         require_complete_tree = FALSE,
+                         logging_console = TRUE,
+                         logging_threshold = "info") {
+  ## TODO: there's an inconsistency here between the arguments to this
+  ## function using underscores and the arguments to
+  ## 'outpack_config_set' which uses dots. We might take a list of
+  ## options and dots here, which would allow sharing more code. That
+  ## would also allow addition of more options more easily.
+  ##
+  ## TODO: the constraints here need writing out clearly for any other
+  ## implementation that might seek to change them!
   assert_scalar_character(path)
   has_orderly_config <- file.exists(file.path(path, "orderly_config.yml"))
-  if (!has_orderly_config) {
+  if (!has_orderly_config && file.exists(path)) {
     if (!is_directory(path)) {
       stop("'path' exists but is not a directory")
     }
@@ -31,48 +65,36 @@ orderly_init <- function(path, ...) {
       }
     }
   }
-  if (file.exists(file.path(path, ".outpack"))) {
+
+  path_outpack <- file.path(path, ".outpack")
+  if (file.exists(path_outpack)) {
+    ## TODO: check no args given, and if given check that they don't
+    ## differ from the versions that were baked in.
     root <- outpack_root_open(path, FALSE)
   } else {
-    root <- outpack_init(path, ...)
+    config <- config_new(path_archive, use_file_store, require_complete_tree,
+                         logging_console, logging_threshold)
+    fs::dir_create(path_outpack)
+    fs::dir_create(file.path(path_outpack, "metadata"))
+    fs::dir_create(file.path(path_outpack, "location"))
+    config_write(config, path)
+    root <- outpack_root$new(path)
+    outpack_log_info(root, "init", path, "orderly2::outpack_init")
   }
+
   if (!has_orderly_config) {
     writeLines(empty_config_contents(), file.path(path, "orderly_config.yml"))
   }
+
   invisible(orderly_root(path, locate = FALSE, environment()))
 }
 
 
-## In orderly, there's quite a bit more to read here, coping with the
-## configuration (orderly_config.yml) which also marks the root.
-##
-## In orderly, the key fields in the configuration are:
-##
-## Things that will be removed
-##
-## * destination - orderly's internal db, now ignored
-## * tags - removed as unused
-##
-## Things that will be entirely reworked
-##
-## * remote - configuration of remotes, will get a full change as this
-##   has lots of issues as is, and because this interacts with
-##   outpack's 'location' support
-## * changelog - will get overhauled because it's not clear that it works
-##   well at the moment
-##
-## Things that will probably come in without too much change:
-##
-## * fields - custom fields
-## * vault - vault configuration
-## * global_resources - might be the first bit to come back in?
-## * database - lower priority, as only VIMC and everything else must work first
-## * minimum_orderly_version - the required version
-orderly_root <- function(root, locate, call = NULL) {
-  if (inherits(root, "orderly_root")) {
-    return(root)
+orderly_root <- function(path, locate, call = NULL) {
+  if (inherits(path, "orderly_root")) {
+    return(path)
   }
-  if (!file.exists(file.path(root, ".outpack"))) {
+  if (!file.exists(file.path(path, ".outpack"))) {
     cli::cli_abort(
       c(sprintf("orderly directory '%s' not initialised", root),
         x = "Did not find an '.outpack' directory within path",
@@ -80,16 +102,11 @@ orderly_root <- function(root, locate, call = NULL) {
         i = "See ?orderly_init for more arguments to this function"),
       call = call)
   }
-  root <- outpack_root_open(root, locate)
-  ## NOTE: it's can't be changed yet, but core.path_archive cannot be
-  ## "draft" for this to work well.
-  path <- root$path
-  config <- orderly_config(path)
-  ret <- list(outpack = root, config = config, path = path)
-  class(ret) <- "orderly_root"
-  ret
+  root <- outpack_root_open(path, locate)
+  root$config$orderly <- orderly_config(path)
+  class(root) <- c("orderly_root", class(root))
+  root
 }
-
 
 
 empty_config_contents <- function() {
