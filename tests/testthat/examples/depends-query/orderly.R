93cffc5409e56f0ab81c51ecb25da9aab4f3b6d4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-08-02 13:51:44 +0100

    Add test case

---
+++ tests/testthat/examples/depends-query/orderly.R
@@ -0,0 +1,10 @@
+orderly2::orderly_parameters(a = NULL, b = NULL, c = NULL)
+orderly2::orderly_dependency(
+  "parameters",
+  paste("latest(parameter:a == this:a &&",
+        "parameter:b == this:b &&",
+        "parameter:c == this:c)"),
+  c(input.rds = "data.rds"))
+orderly2::orderly_artefact("Processed data", "result.rds")
+d <- readRDS("input.rds")
+saveRDS(lapply(d, function(x) x * 2), "result.rds")
