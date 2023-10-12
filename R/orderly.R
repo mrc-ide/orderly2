de608615091db4674b196034003341e1579afb97
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-10-12 18:14:51 +0100

    Basic orderly_new implementation
    No real template support, but bits in place in case people want
    that back.

--- R/orderly.R
+++ R/orderly.R
@@ -23,3 +23,57 @@ orderly_list_src <- function(root = NULL, locate = TRUE) {
   pos <- fs::dir_ls(file.path(root_path, "src"), type = "directory")
   basename(pos)[file_exists(file.path(pos, "orderly.R"))]
 }
+
+
+##' Create a new empty report.
+##'
+##' @title Create a new report
+##'
+##' @param name The name of the report
+##'
+##' @param template The template to use. The only acceptable values
+##'   for now are `NULL` (uses the built-in default) and `FALSE` which
+##'   suppresses any default content.  We may support customisable
+##'   templates in future - let us know if this would be useful.
+##'
+##' @param force Create an `orderly.R` file within an existing
+##'   directory `src/<name>`; this may be useful if you have already
+##'   created the directory and some files first but want help
+##'   creating the orderly file.
+##'
+##' @inheritParams orderly_run
+##'
+##' @return Nothing, called for its side effects only
+##' @export
+orderly_new <- function(name, template = NULL, force = FALSE,
+                        root = NULL, locate = TRUE) {
+  root <- root_open(root, locate, require_orderly = TRUE, call = environment())
+  dest <- file.path(root$path, "src", name)
+
+  if (file.exists(file.path(dest, "orderly.R"))) {
+    cli::cli_abort("'src/{name}/orderly.R' already exists")
+  }
+  if (file.exists(dest) && !fs::is_dir(dest)) {
+    cli::cli_abort(
+      c("'src/{name}' already exists, but is not a directory",
+        i = "This file really should not be here, you might need to tidy up"))
+  }
+  if (length(dir(dest, all.files = TRUE, no.. = TRUE)) > 0 && !force) {
+    cli::cli_abort(
+      c("'src/{name}/' already exists and contains files",
+        i = paste("If you want to add an orderly.R to this directory,",
+                  "rerun {.code orderly_new()} with {.code force = TRUE}")))
+  }
+
+  if (is.null(template)) {
+    contents <- readLines(orderly2_file("template/default.R"))
+  } else if (isFALSE(template)) {
+    contents <- character()
+  } else {
+    cli::cli_abort("'template' must be 'NULL' or 'FALSE' for now")
+  }
+
+  fs::dir_create(dest)
+  writeLines(contents, file.path(dest, "orderly.R"))
+  cli::cli_alert_success("Created 'src/{name}/orderly.R'")
+}
--- tests/testthat/test-orderly.R
+++ tests/testthat/test-orderly.R
@@ -29,3 +29,75 @@ test_that("no candidates returns empty character vector", {
   expect_equal(withr::with_dir(path, orderly_list_src()), character())
   expect_equal(orderly_list_src(path), character())
 })
+
+
+test_that("can create empty orderly report", {
+  path <- test_prepare_orderly_example(character())
+  expect_message(
+    orderly_new("foo", root = path),
+    "Created 'src/foo/orderly.R'")
+  path_orderly <- file.path(path, "src", "foo", "orderly.R")
+  expect_true(file.exists(path_orderly))
+  txt <- readLines(path_orderly)
+  expect_match(txt[[1]], "This is an orderly script")
+})
+
+
+test_that("can create a totally blank orderly report", {
+  path <- test_prepare_orderly_example(character())
+  expect_message(
+    orderly_new("foo", template = FALSE, root = path),
+    "Created 'src/foo/orderly.R'")
+  path_orderly <- file.path(path, "src", "foo", "orderly.R")
+  expect_true(file.exists(path_orderly))
+  expect_equal(readLines(path_orderly), character())
+})
+
+
+test_that("error if orderly.R exists already", {
+  path <- test_prepare_orderly_example("data")
+  expect_error(orderly_new("data", root = path),
+               "'src/data/orderly.R' already exists")
+  expect_error(orderly_new("data", force = TRUE, root = path),
+               "'src/data/orderly.R' already exists")
+})
+
+
+test_that("error if a non-directory file is found in the src dir", {
+  path <- test_prepare_orderly_example(character())
+  file.create(file.path(path, "src", "foo"))
+  err <- expect_error(
+    orderly_new("foo", template = FALSE, root = path),
+    "'src/foo' already exists, but is not a directory")
+  expect_equal(
+    err$body,
+    c(i = "This file really should not be here, you might need to tidy up"))
+})
+
+
+test_that("allow creation of orderly.R in existing dir if force is given", {
+  path <- test_prepare_orderly_example(character())
+  fs::dir_create(file.path(path, "src", "foo"))
+  file.create(file.path(path, "src", "foo", "bar"))
+  err <- expect_error(
+    orderly_new("foo", root = path),
+    "'src/foo/' already exists and contains files")
+  expect_equal(
+    err$body,
+    c(i = paste("If you want to add an orderly.R to this directory,",
+                "rerun `orderly_new()` with `force = TRUE`")))
+  expect_message(
+    orderly_new("foo", force = TRUE, root = path),
+    "Created 'src/foo/orderly.R'")
+  expect_true(file.exists(file.path(path, "src/foo/orderly.R")))
+})
+
+
+test_that("allow creation of orderly.R in existing empty dir", {
+  path <- test_prepare_orderly_example(character())
+  fs::dir_create(file.path(path, "src", "foo"))
+  expect_message(
+    orderly_new("foo", root = path),
+    "Created 'src/foo/orderly.R'")
+  expect_true(file.exists(file.path(path, "src/foo/orderly.R")))
+})
