3045d80d737c84246f0d41e3d555593b87e7b8e5
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-05-02 10:58:39 +0100

    Add tests for parameter prompting

--- tests/testthat/test-interactive.R
+++ tests/testthat/test-interactive.R
@@ -133,3 +133,33 @@ test_that("can error when interactive parameters are invlid", {
     "Invalid input for parameter 'foo'")
   expect_equal(err$body[1], c(i = "Must be a simple boolean, number or string"))
 })
+
+
+test_that("error if prompting for parameters in non-interactive session", {
+  rlang::local_interactive(FALSE)
+  expect_error(
+    prompt_parameters_interactive(list(a = NULL)),
+    "Can't interactively fetch parameters in non-interactive session")
+})
+
+
+test_that("prompt for parameters interactively", {
+  rlang::local_interactive(TRUE)
+  mock_get <- mockery::mock(1, 2, 3)
+  testthat::local_mocked_bindings(get_parameter_interactive = mock_get)
+  expect_message(
+    res <- prompt_parameters_interactive(
+      list(a = NULL, b = 20, c = NULL), NULL),
+    "Please enter values for 3 parameters")
+  expect_equal(res, strict_list(a = 1, b = 2, c = 3, .name = "parameters"))
+  mockery::expect_called(mock_get, 3)
+  expect_equal(
+    mockery::mock_args(mock_get)[[1]],
+    list("a", NULL, NULL))
+  expect_equal(
+    mockery::mock_args(mock_get)[[2]],
+    list("b", 20, NULL))
+  expect_equal(
+    mockery::mock_args(mock_get)[[3]],
+    list("c", NULL, NULL))
+})
