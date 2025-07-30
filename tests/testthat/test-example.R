ff41fe8c8d1df0fb3c1967f5c7aa9b92594b50b7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-07-30 07:57:42 +0100

    Fix tests

--- tests/testthat/test-example.R
+++ tests/testthat/test-example.R
@@ -1,13 +1,13 @@
 test_that("can use custom path", {
   path <- withr::local_tempfile()
-  res <- suppressMessages(orderly_example("simple", dest = path))
+  res <- suppressMessages(orderly_example(example = "simple", dest = path))
   expect_equal(res, path)
   expect_equal(orderly_list_src(root = path), "data")
 })
 
 
 test_that("can set config", {
-  res <- suppressMessages(orderly_example("simple",
+  res <- suppressMessages(orderly_example(example = "simple",
                                           use_file_store = TRUE,
                                           path_archive = NULL))
   config <- orderly_config(res)
@@ -18,6 +18,6 @@ test_that("can set config", {
 
 test_that("error if path exists", {
   path <- withr::local_tempdir()
-  expect_error(orderly_example("simple", dest = path),
+  expect_error(orderly_example(example = "simple", dest = path),
                "The path '.+' must not exist")
 })
