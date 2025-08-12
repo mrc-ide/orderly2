1096956e75f9de1bf002142a97d50ca8c0650c86
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-08-12 20:30:09 +0100

    Fix test

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -31,7 +31,7 @@ test_that("environment files must be really simple", {
 test_that("can validate minimum required version", {
   expect_error(
     orderly_config_validate_minimum_orderly_version("1.4.5", "orderly.yml"),
-    "Detected old orderly version, you need to migrate to orderly",
+    "Detected old orderly project, you need to migrate to orderly 2",
     fixed = TRUE)
   expect_error(
     orderly_config_validate_minimum_orderly_version("99.0.0", "orderly.yml"),
