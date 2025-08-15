d6b021437a8e3cb59b21b620837d8be0f742bcc7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-08-15 16:26:10 +0100

    Add ability to load compatibility

--- R/compat.R
+++ R/compat.R
@@ -11,8 +11,51 @@
 ##
 ## 3. we should create a real, installable, package that wrap over
 ## orderly (inst/orderly2, we'll pop this onto the universe).
-
-
 load_orderly2_support <- function() {
-  
+  if (isTRUE(cache$orderly2_support)) {
+    return(invisible())
+  }
+  if (isTRUE(getOption("orderly_disable_orderly2_compat", FALSE))) {
+    cli::cli_abort(
+      paste("Not loading orderly2 support, as this is disabled by the option",
+            "'orderly_disable_orderly2_compat'"))
+  }
+  if (isNamespaceLoaded("orderly2")) {
+    if (getNamespaceVersion("orderly2") != "1.99.99") {
+      cli::cli_abort(
+        c("Can't load orderly2 compatibility as orderly2 is loaded",
+          i = paste("You have an old version of 'orderly2' installed and",
+                    "loaded; please try again in a fresh session, perhaps",
+                    "after removing 'orderly2"),
+          i = 'Try {.code remove.packages("orderly2")}'))
+    }
+  } else {
+    installed <- orderly2_installed_version()
+    withr::local_options(orderly2.nowarn = TRUE)
+    if (installed$installed && installed$version == "1.99.99") {
+      loadNamespace("orderly2")
+    } else {
+      pkgload::load_all(orderly_file("orderly2"),
+                        export_all = FALSE,
+                        export_imports = FALSE,
+                        attach = FALSE,
+                        helpers = FALSE,
+                        attach_testthat = FALSE,
+                        quiet = TRUE)
+    }
+  }
+  cache$orderly2_support <- TRUE
+  invisible()
+}
+
+
+orderly2_installed_version <- function() {
+  if (is.null(cache$orderly2_version)) {
+    version <- tryCatch(
+      utils::packageVersion("orderly2"),
+      error = function(e) NULL)
+    cache$orderly2_version <- list(installed = !is.null(version),
+                                   version = version)
+  }
+  cache$orderly2_version
 }
