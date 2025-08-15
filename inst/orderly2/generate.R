e0b57e1c89f1d9ca2a603a6b04cf58e45a1bbf8a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-08-15 16:44:37 +0100

    Simplify dummy package

--- inst/orderly2/generate.R
+++ inst/orderly2/generate.R
@@ -1,9 +1,14 @@
 #!/usr/bin/env Rscript
+
+# Ensure this script is run from the 'inst/orderly2' subdirectory:
 stopifnot(read.dcf("DESCRIPTION", "Package") == "orderly2")
+
+# We will not have to rerun this very often, if ever again, because
+# people using 'orderly2::fn' in their code won't expect new orderly1
+# functions to appear.  Nonetheless, the file *was* automatically
+# generated so we might as well save the script
 nms <- getNamespaceExports("orderly")
 writeLines(
-  c(sprintf("#' @importFrom orderly %s", nms),
-    sprintf("#' @export %s", nms),
-    "NULL"),
-  "R/imports.R")
-devtools::document()
+  c(sprintf("importFrom(orderly, %s)", nms),
+    sprintf("export(%s)", nms)),
+  "NAMESPACE")
